ee343cf43d03fd818b451a16eb3bbf3a86221c13
darrdv <darr.dv@gmail.com>
2022-09-23 23:15:14 -0400

    Added Some Game Logic

--- dice.js
+++ dice.js
@@ -2,7 +2,37 @@ let player1Score = 0;
 let player2Score = 0;
 let player1Turn = true;
 
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1ScoreBoard = document.getElementById("player1ScoreBoard")
+const player2ScoreBoard = document.getElementById("player2ScoreBoard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
 /* Hook up a click event listener to the Roll Dice Button. */
 rollBtn.addEventListener("click", function() {
   const randomNumber = Math.floor(Math.random() * 6) + 1
+
+  if(player1Turn){
+    player1Dice.textContent = randomNumber;
+    player1Score += randomNumber;
+    player1ScoreBoard.textContent = player1Score;
+
+  }else{
+    player2Dice.textContent = randomNumber;
+    player2Score += randomNumber;
+    player2ScoreBoard.textContent = player2Score;
+  }
+
+  if(player1Score >= 20){
+
+  }
+  else if(player2Score >= 20){
+
+  }
+
+  player1Turn = !player1Turn
+
 });
