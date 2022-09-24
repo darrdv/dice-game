6ce3a6c639363c521f066b64f2c6e70cba5cf42d
darrdv <darr.dv@gmail.com>
2022-09-23 22:18:19 -0400

    Added Event Listener

---
+++ dice.js
@@ -0,0 +1,8 @@
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
+
+/* Hook up a click event listener to the Roll Dice Button. */
+rollBtn.addEventListener("click", function() {
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+});
