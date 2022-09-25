35970b64e67b6c85f3273e6fe055e73ed17a177d
darrdv <darr.dv@gmail.com>
2022-09-24 23:32:25 -0400

    Added reset game event listener

--- dice.js
+++ dice.js
@@ -11,6 +11,26 @@ const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
+function showDisplayButton() {
+  rollBtn.style.display = "none"
+  resetBtn.style.display = "block"
+}
+
+function reset(){
+  resetBtn.style.display = "none"
+  rollBtn.style.display = "block"
+  player1Score = 0
+  player2Score = 0
+  player1Turn = true
+  message.textContent = "Player 1 Turn"
+  player2Dice.classList.remove("active")
+  player1Dice.classList.add("active")
+  player1Dice.textContent = '-'
+  player2Dice.textContent = '-'
+  player1Scoreboard.textContent = '0'
+  player2Scoreboard.textContent = '0'
+}
+
 /* Hook up a click event listener to the Roll Dice Button. */
 rollBtn.addEventListener("click", function() {
   const randomNumber = Math.floor(Math.random() * 6) + 1
@@ -19,24 +39,29 @@ rollBtn.addEventListener("click", function() {
     player1Dice.textContent = randomNumber;
     player1Score += randomNumber;
     player1ScoreBoard.textContent = player1Score;
-    player1Dice.classList.remove("active")
-    player2Dice.classList.add("active")
+    player1Dice.classList.remove("active");
+    player2Dice.classList.add("active");
+    message.textContent = "Player 2 Turn";
 
   }else{
     player2Dice.textContent = randomNumber;
     player2Score += randomNumber;
     player2ScoreBoard.textContent = player2Score;
-    player2Dice.classList.remove("active")
-    player1Dice.classList.add("active")
+    player2Dice.classList.remove("active");
+    player1Dice.classList.add("active");
+    message.textContent = "Player 1 Turn";
   }
 
-  if(player1Score >= 20){
-
-  }
-  else if(player2Score >= 20){
-
-  }
+  if (player1Score >= 20) {
+    message.textContent = "Player 1 has won! 🥳";
+    showDisplayButton();
+  } else if (player2Score >= 20) {
+    message.textContent = "Player 2 has won! 🎉";
+    showDisplayButton();
+}
 
   player1Turn = !player1Turn
 
 });
+
+resetBtn.addEventListener("click", reset);
